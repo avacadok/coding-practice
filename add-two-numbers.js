a485f6fa8906eab40b9114a98391ac2c87b05acf
avacadok <avakuang8@gmail.com>
2022-10-12 17:53:01 -0700

    link the home button on nav bar with home page

---
+++ add-two-numbers.js
@@ -0,0 +1,18 @@
+/*given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807.
+*/
+
+// const addTwoNumbers = (l1, l2) => {
+
+// }
+
+var addTwoNumbers = function (l1, l2) {
+
+};
+
+console.log(addTwoNumbers([2,4,5], [5,6,4]))
